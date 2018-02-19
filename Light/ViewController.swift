0d562db31cb5fe2e009b0edc6d45b2df7e087258
Jan Marten Sevenster <janmarten@jans-macbook-pro.local>
2018-02-19 16:13:34 +0100

    App done

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -2,17 +2,25 @@
 //  ViewController.swift
 //  Light
 //
-//  Created by Jan Marten Sevenster on 10/02/2018.
+//  Created by Jan Marten Sevenster on 09/02/2018.
 //  Copyright © 2018 John Appleseed. All rights reserved.
 //
 
 import UIKit
-
+var lightOn = true
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
     }
 
     override func didReceiveMemoryWarning() {
